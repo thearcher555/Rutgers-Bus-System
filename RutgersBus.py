e86bc486878c936c0b9e7291bb199df959f3d68d
cvoy138 <cvoy138@gmail.com>
2022-04-27 22:17:59 -0400

    added stop object and started route doc

--- RutgersBus.py
+++ RutgersBus.py
@@ -11,9 +11,17 @@ class Bus:
         self.num = num
         self.bus_status = str(bus_status)
 class Route:
-    def __init__(self, num_buses, stops = [],):
+    def __init__(self, name, num_buses, stops = [],):
+        self.name = str(name)
         self.stops = []
         self.num_buses = num_buses
+class Stop:
+    def __init__(self, campus, num_buses, time2stops = {}):
+        self.campus = str(campus)
+        self.num_buses = 0
+        self.time2stops = {}
 
+
+        
 
     
