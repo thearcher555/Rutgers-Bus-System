888c42058452ffa3bd655c8983a9c795e42b3ed2
cvoy138 <cvoy138@gmail.com>
2022-04-27 21:58:58 -0400

    created some objects

--- RutgersBus.py
+++ RutgersBus.py
@@ -4,4 +4,16 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 import tkinter as tk
-from functools import partial
+from functools import partial3
+
+class Bus:
+    def _init__(self, num, bus_status):
+        self.num = num
+        self.bus_status = str(bus_status)
+class Route:
+    def __init__(self, num_buses, stops = [],):
+        self.stops = []
+        self.num_buses = num_buses
+
+
+    
