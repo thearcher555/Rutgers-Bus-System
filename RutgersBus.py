b29636f99c7d936cf18cf3ea47235fb488b3fa08
cvoy138 <cvoy138@gmail.com>
2022-04-27 22:59:16 -0400

    minor edits

--- RutgersBus.py
+++ RutgersBus.py
@@ -7,20 +7,21 @@ import tkinter as tk
 from functools import partial3
 
 class Bus:
-    def _init__(self, num, bus_status):
+    def _init__(self, num, bus_status, route):
         self.num = num
         self.bus_status = str(bus_status)
 class Route:
-    def __init__(self, name, num_buses, stops = [],):
+    def __init__(self, name, num_buses, stops = [], buses = []):
         self.name = str(name)
         self.stops = []
+        self.buses = []
         self.num_buses = num_buses
 class Stop:
     def __init__(self, campus, num_buses, time2stops = {}):
         self.campus = str(campus)
         self.num_buses = 0
         self.time2stops = {}
-
+#  "AT STOP" , "IN TRANSIT", "TAKING BREAKING"
 
         
 
