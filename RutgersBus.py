bb18fd6504a7a0981696461f466c00fffe15549f
Arthur Lai <laiarthurfeng@gmail.com>
2022-04-28 00:50:47 -0400

    finished readRoutes function, small changes to constructor of classes

--- RutgersBus.py
+++ RutgersBus.py
@@ -1,39 +1,79 @@
+#!/usr/bin/python3
 from configparser import MAX_INTERPOLATION_DEPTH
 from pickle import FALSE, TRUE
 import random
-import numpy as np
-import matplotlib.pyplot as plt
-import tkinter as tk
-from functools import partial3
+#import numpy as np
+#import matplotlib.pyplot as plt
+#import tkinter as tk
+#from functools import partial3
 
-# status: "AT STOP" , "IN TRANSIT", "TAKING BREAKING"
+idCount = 0
+idBus = 0
+
+# status: "S" (at a stop) , "T" (in transit), "B" (taking a break)
 class Bus:
-    def _init__(self, num, bus_status, route):
+    def __init__(self, num, bus_status, route):
+        self.route = route
         self.num = num
         self.bus_status = str(bus_status)
+
 class Route:
     def __init__(self, name, num_buses, stops = [], buses = []):
         self.name = str(name)
         self.stops = []
         self.buses = []
         self.num_buses = num_buses
+
+        for x in range(self.num_buses):
+            global idBus
+            self.buses.append(Bus(idBus, "B", self.name))
+            idBus += 1
+
 class Stop:
-    def __init__(self, campus, num_buses, time2stops = {}):
-        self.campus = str(campus)
-        self.num_buses = 0
-        self.time2stops = {}
+    def __init__(self, name):
+        self.name = name
+        self.id = 0
 
-
-def readRoutes():
+def readRoutes(allStops = []):
     f = open("BusRoutes.txt", "r")
     lines = f.readlines()
     allRoutes = []
 
     for x in lines:
+        x = x.strip()
         tempLine = x.split(';')
-        tempRoute = Route(tempLine[0], )
+        routename = tempLine[0]
+        tempLine = tempLine[1:]
 
+        tempRoute = Route(routename, 5, [], [])
+        
+        for element in tempLine:
+            tempStop = Stop(element)
 
+            found = 0
+            for stops in allStops:
+                if stops.name == tempStop.name:
+                    found = 1
+                    break
+
+            if found == 0:
+                global idCount
+                tempStop.id = idCount
+                idCount += 1
+                allStops.append(tempStop)
+                tempRoute.stops.append(tempStop)
+            else:
+                for stops in allStops:
+                    if stops.name == tempStop.name:
+                        tempStop.id = stops.id
+                tempRoute.stops.append(tempStop)
+                           
+        allRoutes.append(tempRoute)
+    
     return allRoutes
 
+allStops = []
+readRoutes(allStops)
+for x in allStops:
+    print("name: " + str(x.name) + "    id: " + str(x.id))
     
